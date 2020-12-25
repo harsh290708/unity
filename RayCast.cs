b74dc0a10d59370a2324d33ec41880beb3ca7a88
harsh290708 <74399658+harsh290708@users.noreply.github.com>
2020-12-25 16:51:07 +0530

    Add files via upload

---
+++ RayCast.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+   public Camera Playercamera;
+   public float FireRate = 10f;
+   public float timeBetweenNextShot;
+   public float Damage = 20f;
+
+
+   void Update(){
+       if(Input.GetButton("Fire1")&& Time.time >= timeBetweenNextShot){
+           timeBetweenNextShot = Time.time + 1f/FireRate;
+        weapon();
+       }
+   }
+   void weapon(){
+   RaycastHit hit;
+   if(Physics.Raycast(Playercamera.transform.position , Playercamera.transform.forward,out hit )){
+       Health enemy = hit.transform.GetComponent<Health>();
+ enemy.Damage(Damage);
+   }
+   }
+}
